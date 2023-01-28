36281ea80c038ec444b112f627e8158c7fff14a0
sm-alamin <sma.fkml@gmail.com>
2023-01-28 22:56:14 +0600

    type of variable is done

---
+++ type-of-variable.js
@@ -0,0 +1,30 @@
+var fullName = "Mohammad Al-Amin";
+console.log(fullName);
+console.log(typeof(fullName));
+
+var age = 27;
+console.log(age);
+console.log(typeof(age));
+
+var isMarried = false;
+console.log(isMarried);
+console.log(typeof(isMarried));
+
+var profession;
+console.log(profession);
+console.log(typeof(profession));
+
+var cars = ["Saab", "Volvo", "BMW"];
+
+console.log(cars);
+console.log(typeof(cars));
+
+var person = {
+    firstName:"John",
+     lastName:"Doe"
+    };
+    console.log(person);
+    console.log(typeof(person));
+var status = null;
+console.log(status);
+console.log(typeof(status));
