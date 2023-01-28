3602c590749e0091aca18b8d67b4ff00f8801007
sm-alamin <sma.fkml@gmail.com>
2023-01-29 00:13:36 +0600

    loop  is done

---
+++ loop7.js
@@ -0,0 +1,8 @@
+var usedPhone = ["Nokia", "Samsung", "Xiaomi", "Motorola", "Iphone"];
+
+var i = 0;
+while (i < usedPhone.length) {
+    var showOutput =usedPhone[i];
+    console.log(showOutput);
+    i++;
+}
