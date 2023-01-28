9b9e97525252122856ae2f265f5ae371501f047d
sm-alamin <sma.fkml@gmail.com>
2023-01-28 23:21:14 +0600

    primitiive vs non primitive is done

---
+++ primitive-vs-non-primitive.js
@@ -0,0 +1,6 @@
+var primitiveData = "1. Primitive Data types are predefined \n 2. Primitive Data types will have certain values \n 3. Size depends on type on data structure \n 4. Examples are numbers, string \n 5. It can start with lowercase"
+
+var nonPrimitiveData = "1. Non-Primitive data types are created by programmer \n 2. Non-Primitive data types can be NULL \n 3. Size are not fixed \n 4. Example are Array, Linked List 5. It can start with uppercase ";
+
+console.log(primitiveData);
+console.log(nonPrimitiveData);
