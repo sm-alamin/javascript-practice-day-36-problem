4bdef86a7a18e36a2ee11c7055b28cb725319d3c
sm-alamin <sma.fkml@gmail.com>
2023-01-28 22:09:00 +0600

    javascript work process is done

---
+++ how-does-it-work.js
@@ -0,0 +1,5 @@
+var wayOfWork1 = " In contrast, JavaScript has no compilation step. Instead, an interpreter in the browser reads over the JavaScript code, interprets each line, and runs it.";
+var wayOfWork2 = "More modern browsers use a technology known as Just-In-Time (JIT) compilation, which compiles JavaScript to executable bytecode just as it is about to run.";
+
+var showWayOfWorkJavascript = wayOfWork1 + wayOfWork2 ;
+console.log(showWayOfWorkJavascript);
