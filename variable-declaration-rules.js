1ac023e16842cf3a5c1d48c1db41671b78f344ce
sm-alamin <sma.fkml@gmail.com>
2023-01-28 22:38:16 +0600

    variable declaration is done

---
+++ variable-declaration-rules.js
@@ -0,0 +1,8 @@
+var rule1 = "1. variable name must start with a letter (a to z or A to Z), underscore( _ ), or dollar( $ ) sign. \n";
+var rule2 = "2. After first letter we can use digits (0 to 9), for example value1.\n";
+var rule3 = "3. Javascript variables are case sensitive, for example x and X are different variables.\n";
+var rule4 ="4. We should use camel case to declare a variable \n";
+var rule5 = "5. We can not use number at the start of variable";
+
+var variableRules = rule1 + rule2 + rule3 + rule4 + rule5;
+console.log(variableRules);
