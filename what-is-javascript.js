28021ed1cb19ae2979fc33eb8dce58604bd237f6
sm-alamin <sma.fkml@gmail.com>
2023-01-28 22:00:10 +0600

    the definition is done

---
+++ what-is-javascript.js
@@ -0,0 +1,10 @@
+var definition = "Javascript is a "
+var feature1 = "lightweight "
+var feature2 = "interpreted ";
+var feature3 = "or just-in-time ";
+var feature4 = "compiled ";
+var feature5 = "programming language ";
+var feature6 = "with first-class functions ";
+
+var javascript = definition + feature1 + feature2 + feature3 +feature4 + feature5 + feature6;
+console.log(javascript);
