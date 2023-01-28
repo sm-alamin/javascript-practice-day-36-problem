af94aca0d54076359101b7022295ab7b0d98bb3b
sm-alamin <sma.fkml@gmail.com>
2023-01-29 00:07:06 +0600

    loop6 practice is done

---
+++ loop6.js
@@ -0,0 +1,6 @@
+var learned = ["Html", "CSS", "Bootstrap", "Tailwind", "Javascript"];
+
+for (var index = 0; index < learned.length; index++) {
+    var show = learned[index];
+    console.log(show);
+}
