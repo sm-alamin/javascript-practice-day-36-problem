9a4b2aa6d80f41c1d1115e17fa1247b46f7b1145
sm-alamin <sma.fkml@gmail.com>
2023-01-29 12:35:28 +0600

    short hand is done

---
+++ short-hand.js
@@ -0,0 +1,24 @@
+var apples = 100;
+apples += 5;
+console.log(apples);
+
+var oranges =100;
+oranges -= 6;
+console.log(oranges);
+
+var pens = 50;
+pens *= 3;
+console.log(pens);
+
+var watermelon = 50;
+watermelon /= 3;
+var showTwoDecimal = watermelon.toFixed(2);
+console.log(showTwoDecimal);
+
+var pineapples = 89;
+pineapples ++;
+console.log(pineapples);
+
+var notebook = 50;
+notebook --;
+console.log(notebook);
