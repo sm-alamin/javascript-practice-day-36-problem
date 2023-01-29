2d791f73e2280a3f6df2fc1929cf5c0ee2daba06
sm-alamin <sma.fkml@gmail.com>
2023-01-29 13:47:42 +0600

    all condition releted problem is solved

---
+++ all-condition-releted-problem.js
@@ -0,0 +1,23 @@
+//normal condition
+var amount = 65000;
+
+if (amount > 80000) {
+    console.log("Ami mac kinbo");
+}else if (amount > 60000) {
+    console.log("Ami gaming laptop kinbo");
+}else if (amount > 40000) {
+    console.log("Ami lenovo yoga kinbo");
+}else{
+    console.log("Ami mobile phone kinbo");
+}
+
+//complex conditions
+var profession = "Ami Teacher Hobo";
+var ntrcaPass = true;
+var meritList = 50;
+
+if (ntrcaPass == true && meritList < 100 ) {
+    console.log(profession);
+}else{
+    console.log("amake onno kono profession khujte hobe");
+}
